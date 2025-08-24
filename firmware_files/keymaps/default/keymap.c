35b5dd7de775b0f550a343a15f421b0896b8fec6
Joel Ramin Neuhaus-Khatami <jokha001@gmail.com>
2025-08-25 01:00:29 +0200

    firmware + function layering

--- firmware_files/keymaps/default/keymap.c
+++ firmware_files/keymaps/default/keymap.c
@@ -18,11 +18,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *       └───┴─────┴───────────────────────────┴─────┴───┘
      */
     [0] = LAYOUT_JXT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),
-                          KC_LALT, KC_LGUI,                   KC_SPC,                             KC_RGUI, KC_RALT
+        KC_F1, KC_F2,       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, KC_CAPS,        KC_VOLD, KC_VOLU, KC_MUTE,
+        KC_F3, KC_F4,       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_BSLS, KC_BSPC,       KC_PGUP, KC_HOME, KC_INS,
+        KC_F5, K_F6,        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_RBRC, KC_ENT,        KC_PGDN, KC_END, KC_DEL,
+        KC_F7, K_F8,        FKC_LSFT, KC_GRV, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),         KC_TAB, KC_UP, KC_LALT,
+        KC_F9, K_F10,              KC_LALT, KC_LGUI,                   KC_SPC,                             KC_RGUI, KC_RALT,                                    KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     /*
@@ -39,10 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *       └───┴─────┴───────────────────────────┴─────┴───┘
      */
     [1] = LAYOUT_JXT(
-        KC_PWR,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,
-        KC_CAPS, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_UP,   _______, _______,
-        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,          KC_PENT,
-        _______,          _______, _______, _______, _______, _______, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN, _______, _______,
-                          _______, _______,                   _______,                            _______, _______
+        KC_F11, KC_F12,       ________,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,       _______, _______, ________,
+        _______, ________,       _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,       _______, _______, ________,
+        _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,       _______, _______, ________,
+        _______, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         _______, _______, _______,
+        _______, _______,              _______, _______,                   _______,                             _______, _______,                                    KC_MPRV, KC_MPLY, KC_MNXT
+
     )
 };
